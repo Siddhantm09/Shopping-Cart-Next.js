166d53a64f7dccb2ba3601719b22006143f862ec
Siddhant <mahatosiddhant69@gmail.com>
2023-10-11 12:17:25 +0530

    recent

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -3,7 +3,26 @@ import "./cart.css";
 import Image from "next/image";
 
 const cart = () => {
-  return <section className="section"></section>;
+  return (
+    <section className="section">
+      <div className="container">
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+          </tr>
+          <tr>
+            <td>Product Name</td>
+            <td>
+              <input type="number" />
+            </td>
+            <td>50 Rs</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+  );
 };
 
 export default cart;
--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import "./Navbar.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../assets/pngwing.com (2).png";
 const Navbar = () => {
   return (
     <div className="nav-container flex">
       <div className="logo">
         <Link className="" href="/">
-          Logo
+          <Image src={logo} width={80} height={70}></Image>
         </Link>
       </div>
       <div className="links">
         <ul className="flex">
-          <Link href="/signup">Home</Link>
-
           <li className="lists">
+            <Link href="/signup">Home</Link>
+          </li>
+
+          <li className="lists-mob flex">
             <input className="search-box" placeholder="Search..." />
             <button className="search-btn">
               <i class="fa-solid fa-magnifying-glass"></i>
             </button>
           </li>
-          <li className="lists flex">
+          {/* <li className="lists flex">
             <Link href="/signup" className="sign-up-btn">
               Sign up
             </Link>
-          </li>
-          <li className="lists">
+          </li> */}
+          <li className="lists-mob">
             <Link href="/cart">
               <i
                 style={{ fontSize: "20px" }}
